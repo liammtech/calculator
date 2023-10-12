7de7d7612388eeb8e4a91a272d18d50aa076af60
liammtech <liammtech@gmail.com>
2023-10-12 23:07:08 +0100

    Add initial function logic

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-function add() {
-
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-function subtract() {
-
+function subtract(num1, num2) {
+    return num1 - num2;
 }
 
-function multiply() {
-
+function multiply(num1, num2) {
+    return num1 * num2;
 }
 
-function divide() {
+function divide(num1, num2) {
+    return num1 / num2;
+}
 
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+    }
 }
