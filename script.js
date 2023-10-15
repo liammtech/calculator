d83664010092162895e6f45f86669c7181103238
liammtech@gmail.com <liammtech@gmail.com>
2023-10-15 15:23:51 +0100

    Add button id's and case selectors

--- script.js
+++ script.js
@@ -32,22 +32,54 @@ function negate(num1) {
 
 function operate(operator, thisNum, prevNum) {
     switch(operator) {
-        case "add":
-            return add(thisNum, prevNum);
-        case "subtract":
-            return subtract(thisNum, prevNum);
-        case "multiply":
-            return multiply(thisNum, prevNum);
-        case "divide":
-            return divide(thisNum, prevNum);
+        case "percentage":
+            return percentage();
+        case "clear-entry":
+            return clearEntry();
+        case "clear":
+            return clear();
+        case "backspace":
+            return backspace();
         case "invert":
             return invert(thisNum);
         case "square":
             return square(thisNum);
         case "square-root":
             return squareRoot(thisNum);
+        case "divide":
+            return divide(thisNum, prevNum);
+        case "multiply":
+            return multiply(thisNum, prevNum);
+        case "subtract":
+            return subtract(thisNum, prevNum);
+        case "add":
+            return add(thisNum, prevNum);
         case "negate":
             return negate(thisNum);
+        case "decimal":
+            return decimal(thisNum);
+        case "equals":
+            return equals(thisNum);
+        case "zero":
+            return zero(thisNum);
+        case "one":
+            return one(thisNum);
+        case "two":
+            return two(thisNum);
+        case "three":
+            return three(thisNum);
+        case "four":
+            return four(thisNum);
+        case "five":
+            return five(thisNum);
+        case "six":
+            return six(thisNum);
+        case "seven":
+            return seven(thisNum);
+        case "eight":
+            return eight(thisNum);
+        case "nine":
+            return nine(thisNum);
     }
 }
 
